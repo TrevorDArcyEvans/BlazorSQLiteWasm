d603c28e737fed9f9dff4ad490b89a36c0248729
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2021-12-26 10:50:02 +0000

    - initial version

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorSQLiteAsm
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+#if DEBUG
+			// Allow some time for debugger to attach to Blazor framework debugging proxy
+			await Task.Delay(TimeSpan.FromSeconds(2));
+#endif
+
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
