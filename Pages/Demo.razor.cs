aeec5e2189d2e274581e96b486a9672f1e886e0a
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2021-12-27 17:19:55 +0000

    - get SQLite version to show it is working

--- Pages/Demo.razor.cs
+++ Pages/Demo.razor.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 
 public partial class Demo
 {
-  private async Task TestSQLite()
-  {
-    var db = new SqliteConnection("Data Source=DemoData.db");
+  private string _version = "unknown";
 
+  private async Task SQLiteVersion()
+  {
+    await using var db = new SqliteConnection("Data Source=DemoData.db");
     await db.OpenAsync();
+    await using var cmd = new SqliteCommand("SELECT SQLITE_VERSION()", db);
+    _version = (await cmd.ExecuteScalarAsync())?.ToString();
   }
 }
